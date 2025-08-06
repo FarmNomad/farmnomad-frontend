56dfcf8613bfbf7ff5265ea20467bf154c36f7a7
Prime <wolabash@gmail.com>
2025-08-06 22:20:19 +0100

    Feat: Codebase 90%

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
